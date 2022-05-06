7c0a95e2738d87e8840f06e918acfee7354cee2a
施承毅 <harryshih@local>
2022-05-06 14:52:10 +0800

    feat: crud api test OT-54

---
+++ test/crudTest.js
@@ -0,0 +1,85 @@
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = 'http://localhost:3000';
+let should = chai.should();
+describe('crud api', () => {
+  describe('read product table', ()=>{
+    it('it should GET all product', (done) => {
+      chai.request(server)
+          .get('/crud')
+          .end((err, res) => {
+            if (err!=null) {
+              console.log(err);
+            }
+            res.should.have.status(200);
+            res.body.should.be.a('array');
+            // res.body[0].id.should.equal(5);
+            done();
+          });
+    });
+  });
+  describe('create a product ', ()=>{
+    it('it should create a product', (done) => {
+      chai.request(server)
+          .post('/crud')
+          .set('content-type', 'application/x-www-form-urlencoded')
+          .send({method: 'create',
+            product_name: '測試',
+            price: 455,
+            remark: 'Teat',
+          })
+          .end((err, res) => {
+            if (err!=null) {
+              console.log(err);
+            }
+            res.should.have.status(200);
+            res.body.success.should.equal(true);
+            done();
+          });
+    });
+  });
+  describe('update a product ', ()=>{
+    it('it should update a product', (done) => {
+      chai.request(server)
+          .post('/crud')
+          .set('content-type', 'application/x-www-form-urlencoded')
+          .send({method: 'update',
+            product_name: '測試',
+            price: 455,
+            remark: 'Teat',
+            price_update: 455,
+          })
+          .end((err, res) => {
+            if (err!=null) {
+              console.log(err);
+            }
+            res.should.have.status(200);
+            res.body.success.should.equal(true);
+            done();
+          });
+    });
+  });
+  describe('delete a product ', ()=>{
+    it('it should delete a product', (done) => {
+      chai.request(server)
+          .post('/crud')
+          .set('content-type', 'application/x-www-form-urlencoded')
+          .send({method: 'delete',
+            product_name: '測試',
+            price: 455,
+            remark: 'Teat',
+          })
+          .end((err, res) => {
+            if (err!=null) {
+              console.log(err);
+            }
+            res.should.have.status(200);
+            res.body.success.should.equal(true);
+            done();
+          });
+    });
+  });
+});
+
+chai.use(chaiHttp);
