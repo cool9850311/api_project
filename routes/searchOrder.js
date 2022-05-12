f2c5743fa1b2707d50b2dae0049b9c5ca2f37d8a
施承毅 <harryshih@local>
2022-05-12 12:37:27 +0800

    feat: redis cache search order api query data OT-61

--- routes/searchOrder.js
+++ routes/searchOrder.js
@@ -3,11 +3,33 @@ const express = require('express');
 const knex = require('knex')(require('../knexfile'));
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const redis = require('redis');
 const auth = require('./auth/auth');
+const redisClient = redis.createClient({
+  socket: {
+    host: 'redis',
+    port: 6379,
+  },
+});
+
+redisClient.on('error', (err) => {
+  console.log(err);
+  console.log('Error occured while connecting or accessing redis server');
+});
+
+
 router.post('/', auth, async function(req, res) {
   const orderID = req.body.order_id;
   if (orderID == null) {
     res.json({success: false, message: 'missing order_id'});
+    return;
+  }
+  await redisClient.connect();
+  const redisResult = await redisClient.get('Order:' + orderID);
+  if (redisResult!=null) {
+    // console.log(redisClient);
+    res.json(JSON.parse(redisResult));
+    redisClient.quit();
     return;
   }
   const queryString = knex.select('sub_order_table.id', 'order_id', 'product_id', 'price', 'amount', 'sub_order_table.remark')
@@ -20,6 +42,8 @@ router.post('/', auth, async function(req, res) {
         this.on('product.id', '=', 'sub_order_table.product_id');
       });
   const result = await queryString;
+  await redisClient.set('Order:' + orderID, JSON.stringify(result));
+  redisClient.quit();
   res.json(result);
 });
 
